a2fb9a7fc3dc1ddd1ddb58d539213be2e278983c
Jbm170 <juba.messaoudi@lgtlecorbusier.org>
2024-12-11 16:33:19 +0100

    widget.h

--- widget.h
+++ widget.h
@@ -1,28 +1,30 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
-#include <QWidget>
-#include <QTableWidget>  // Inclure pour utiliser QTableWidget
+#include <QWidget>      // Inclure la classe QWidget de Qt pour créer des interfaces graphiques
+#include <QTableWidget> // Inclure pour utiliser QTableWidget, qui permet d'afficher des données sous forme de tableau
 
 namespace Ui {
-class Widget;
+class Widget; // Déclaration anticipée de la classe Widget dans l'espace de noms Ui
 }
 
-class Widget : public QWidget
+class Widget : public QWidget // Déclaration de la classe Widget qui hérite de QWidget
 {
-    Q_OBJECT
+    Q_OBJECT // Macro nécessaire pour utiliser les fonctionnalités de Qt, comme les signaux et les slots
 
 public:
-    explicit Widget(QWidget *parent = nullptr);
-    ~Widget();
+    explicit Widget(QWidget *parent = nullptr); // Constructeur de la classe Widget, avec un paramètre parent optionnel
+    ~Widget(); // Destructeur de la classe Widget
 
 private:
-    Ui::Widget *ui;
+    Ui::Widget *ui; // Pointeur vers l'interface utilisateur générée par Qt Designer
 
     // Déclaration du QTableWidget pour afficher les résultats
-    QTableWidget *tableWidget;
+    QTableWidget *tableWidget; // Pointeur vers un QTableWidget qui affichera les données sous forme de tableau
+
 private slots:
-    void updateNoteInDatabase(QTableWidgetItem* item);
+    // Slot qui sera appelé lorsque l'utilisateur modifie une cellule dans le QTableWidget
+    void updateNoteInDatabase(QTableWidgetItem* item); // Méthode pour mettre à jour une note dans la base de données
 
 };
 
